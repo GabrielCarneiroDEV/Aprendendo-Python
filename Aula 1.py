80f06767ce6b3ce037a4f9812c3f6148b229c8a5
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-05 09:58:49 -0300

    Create Aula 1.py
    testando

---
+++ Aula 1.py
@@ -0,0 +1,16 @@
+num1 = int (input ('digite um valor: '))
+num2= int (input('digite outro valor: '))
+s = num1 + num2
+print ('A soma vale', s)
+
+num1 = int (input ('digite um valor: '))
+num2= int (input('digite outro valor: '))
+s = num1 + num2
+print ('A soma entre' , num1 , 'e' , num2 , 'vale' , s)
+
+num1 = int (input ('digite um valor: '))
+num2= int (input('digite outro valor: '))
+s = num1 + num2
+print ('A soma dos numeros {} e {} vale {}' .format(num1, num2, s))
+
+print('fim')
