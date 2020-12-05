d57cd55836bbf9a9ac47a46d4cb302391d43f41e
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-05 14:33:10 -0300

    Create Desconto.py
    exercicío desconto

---
+++ Desconto.py
@@ -0,0 +1,12 @@
+valor = float (input(' digite o valor do produto desejado: R$'))
+desconto = valor * 5 /100
+vd = valor - desconto
+
+print( 'O valor do produtor com desconto de 5% é R${}' .format (vd))
+
+
+valor = float (input(' Digite o salário do funcionário: R$'))
+aumento = valor * 15 /100
+salário = valor + aumento
+
+print( 'o valor do salário do funcionário com aumento é R${}' .format (salário))
