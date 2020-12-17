f383aed0d3fbe431687b2623e4e4e62ca839bacf
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-16 23:06:03 -0300

    Create medido de area.py

---
+++ medido de area.py
@@ -0,0 +1,11 @@
+L = float (input ('quantos metros de largura possui a sua parede?'))
+A = float (input ('quantos metros de altura possui a sua parede?'))
+
+a = L * A
+
+print (' A sua parede possui {}m².' . format (a))
+
+T = a/2
+
+
+print ('você vai precisar de {} litros de tinta'.format (T))
